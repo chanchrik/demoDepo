6068a43fc68b43e6d7905e99d4af2de7ed48d948
chanchrik <chanchrikpatel@gmail.com>
2022-07-13 04:01:22 +0530

    setup repo

---
+++ src/test/java/com/fe/api/demo/deom/DeomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fe.api.demo.deom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
